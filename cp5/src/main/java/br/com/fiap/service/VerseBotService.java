1d096d275b9d906db5a4de5ea696c91626bdf7da
Patricia Naomi <132932532+patinaomi@users.noreply.github.com>
2024-05-05 14:07:31 -0300

    Update VerseBotService.java

--- cp5/src/main/java/br/com/fiap/service/VerseBotService.java
+++ cp5/src/main/java/br/com/fiap/service/VerseBotService.java
@@ -1,6 +1,7 @@
 package br.com.fiap.service;
 
 import br.com.fiap.api.BibleApiClient;
+import br.com.fiap.model.bo.Validacoes;
 import br.com.fiap.model.bo.UsuarioBO;
 import br.com.fiap.model.dao.UsuarioDao;
 import br.com.fiap.model.dao.VersiculoDao;
@@ -30,7 +31,7 @@ public class VerseBotService {
     private TelegramBot bot;
 
     private Map<String, String> userStates = new HashMap<>(); // Armazena o estado da conversa de cada usuário
-    private Map<String, Usuario> tempUsers = new HashMap<>(); // Armazena temporariamente informações de cada usuário
+    private Map<String, Usuario> tempUsers = new HashMap<>(); // Pra armazenar temporariamente as informações de cada usuário
 
     public VerseBotService(String token, OpenAiService openAiService) {
         bot = new TelegramBot(token);
@@ -56,52 +57,51 @@ public class VerseBotService {
     private void processUpdate(Update update) {
         Message message = update.message();
         if (message != null && message.text() != null && !message.text().isEmpty()) {
-            Chat chat = message.chat();
-            String chatId = String.valueOf(chat.id());
-
+            String chatId = String.valueOf(message.chat().id());
             String userText = message.text();
+            String currentState = userStates.getOrDefault(chatId, "");
 
-            String currentState = userStates.get(chatId); // Obtém o estado atual
-
-            if (userText.equalsIgnoreCase("/start")) {
-                String welcomeMessage = "Bem-vindo ao VerseBot! Use /versiculo para um versículo aleatório ou /devocional para um devocional baseado em um versículo aleatório.";
-                bot.execute(new SendMessage(chatId, welcomeMessage));
-                userStates.put(chatId, null); // Redefine o estado
-
-            } else if (userText.equalsIgnoreCase("/cadastro") || "awaiting_name".equals(currentState)) {
-                if ("awaiting_name".equals(currentState)) {
-                    tempUsers.get(chatId).setNome(userText);
-                    userStates.put(chatId, "awaiting_email");
-                    bot.execute(new SendMessage(chatId, "Digite seu e-mail:"));
-                } else {
+            switch (userText.toLowerCase()) {
+                case "/start":
+                    String welcomeMessage = "Bem-vindo ao VerseBot! Selecione alguma opção do menu no canto inferior esquerdo.";
+                    bot.execute(new SendMessage(chatId, welcomeMessage));
+                    userStates.put(chatId, null);
+                    break;
+                case "/cadastro":
                     userStates.put(chatId, "awaiting_name");
-                    tempUsers.put(chatId, new Usuario());
                     bot.execute(new SendMessage(chatId, "Digite seu nome:"));
-                }
-            } else if ("awaiting_email".equals(currentState)) {
-                tempUsers.get(chatId).setEmail(userText);
-                userStates.put(chatId, "awaiting_password");
-                bot.execute(new SendMessage(chatId, "Digite sua senha:"));
-            } else if ("awaiting_password".equals(currentState)) {
-                Usuario newUser = tempUsers.get(chatId);
-                newUser.setSenha(userText);
-                usuarioBO.inserir(newUser);
-
-                userStates.put(chatId, null); // Reseta o estado da conversa
-                bot.execute(new SendMessage(chatId, "Usuário cadastrado com sucesso!"));
-            } else if (userText.equalsIgnoreCase("/versiculo")) {
-                Versiculo verse = getRandomVerse(3);
-                bot.execute(new SendMessage(chatId, verse.toString()));
-            } else if (userText.equalsIgnoreCase("/devocional")) {
-                Versiculo verse = getRandomVerse(3);
-                String devotional = openAiService.gerarDevocional(verse.toString());
-                bot.execute(new SendMessage(chatId, verse.toString() + "\n\n" + devotional));
-            } else {
-                bot.execute(new SendMessage(chatId, "Por favor, use /versiculo para um versículo aleatório ou /devocional para um devocional baseado em um versículo."));
+                    break;
+                default:
+                    if ("awaiting_name".equals(currentState)) {
+                        if (Validacoes.validarNome(userText)) {
+                            tempUsers.put(chatId, new Usuario()); // Inicializa um novo usuário
+                            tempUsers.get(chatId).setNome(userText);
+                            userStates.put(chatId, "awaiting_email");
+                            bot.execute(new SendMessage(chatId, "Nome válido! Digite seu e-mail:"));
+                        } else {
+                            bot.execute(new SendMessage(chatId, "Nome inválido, tente novamente."));
+                        }
+                    } else if ("awaiting_email".equals(currentState)) {
+                        if (Validacoes.validarEmail(userText)) {
+                            tempUsers.get(chatId).setEmail(userText);
+                            userStates.put(chatId, "awaiting_password");
+                            bot.execute(new SendMessage(chatId, "E-mail válido! Digite sua senha:"));
+                        } else {
+                            bot.execute(new SendMessage(chatId, "E-mail inválido, tente novamente."));
+                        }
+                    } else if ("awaiting_password".equals(currentState)) {
+                        tempUsers.get(chatId).setSenha(userText);
+                        usuarioBO.inserir(tempUsers.get(chatId));
+                        tempUsers.remove(chatId);
+                        userStates.remove(chatId);
+                        bot.execute(new SendMessage(chatId, "Usuário cadastrado com sucesso!"));
+                    } else {
+                        bot.execute(new SendMessage(chatId, "Por favor, use /versiculo para um versículo aleatório ou /devocional para um devocional baseado em um versículo."));
+                    }
+                    break;
             }
         }
     }
-
 
 
     private Versiculo getRandomVerse(int userId) {
